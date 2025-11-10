fc9959700a2f00f484762f7eae2d1ec8d411ab88
sohebakhter <sohebakhterbadhan@gmailcom>
2025-11-10 22:14:53 +0600

    added delete api

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
